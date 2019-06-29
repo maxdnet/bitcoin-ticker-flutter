5a2e01012403303335bf227d051159cd3af76829
Massimiliano Dell’Orco <M.DellOrco@almaviva.it>
2019-06-29 05:10:14 +0200

    Bronze Level !

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+const apiUrl = 'https://apiv2.bitcoinaverage.com/indices/global/ticker/';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +34,50 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  List<DropdownMenuItem> getDropDownCoinList() {
+    List<DropdownMenuItem<String>> currencyList = [];
+
+    for (String item in currenciesList) {
+      currencyList.add(DropdownMenuItem(
+        child: Text(item),
+        value: item,
+      ));
+    }
+    return currencyList;
+  }
+
+  List<Text> getCupertinoCoinList() {
+    List<Text> currencyList = [];
+
+    for (String item in currenciesList) {
+      currencyList.add(Text(item));
+    }
+    return currencyList;
+  }
+}
+
+class CoinNetwork {
+  Future<double> getCoinConversionValue(String coins) async {
+    var ret = 0.0;
+    var url = apiUrl + coins;
+
+    var client = http.Client();
+    try {
+      var response = await client.get(url).timeout(Duration(seconds: 5));
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body);
+        ret = jsonResponse["last"];
+      } else {
+        //print("Request failed with status: ${response.statusCode}.");
+        ret = -1;
+      }
+    } catch (e) {
+      //print(e.toString());
+      ret = -1;
+    } finally {
+      client.close();
+    }
+    return ret;
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +9,55 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  int selectedPickerIndex = 0;
+  CoinData coinData = CoinData();
+  CoinNetwork coinNetwork = CoinNetwork();
+  String conversionRatio = '?';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCoinConversionRatio('USD');
+  }
+
+  CupertinoPicker getCupertinoPicker() {
+    return CupertinoPicker(
+      children: coinData.getCupertinoCoinList(),
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedPickerIndex) {
+        print(selectedPickerIndex);
+      },
+    );
+  }
+
+  DropdownButton<String> getAndroidPicker() {
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: coinData.getDropDownCoinList(),
+        onChanged: (value) async {
+          getCoinConversionRatio(value);
+          setState(() {
+            conversionRatio = '?';
+            selectedCurrency = value;
+          });
+        });
+  }
+
+  void getCoinConversionRatio(String exchangeCurrency) async {
+    var ret =
+        await coinNetwork.getCoinConversionValue('BTC' + exchangeCurrency);
+    setState(() {
+      conversionRatio = (ret > -1) ? ret.toString() : 'NA';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
+        title: Text('Coin Ticker'),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -27,7 +74,7 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  '1 BTC = $conversionRatio $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -42,7 +89,7 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getCupertinoPicker() : getAndroidPicker(),
           ),
         ],
       ),
